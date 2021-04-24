587e6176d2eb5fdf83aee2da8a1b6f777a62c804
Tea <icepie.dev@gmail.com>
2021-04-24 20:24:28 +0800

    del openssl

--- compile.sh
+++ compile.sh
@@ -22,5 +22,5 @@ cd openwrt-imagebuilder-${version}-ramips-mt7620.Linux-x86_64
 
 cp ../openwrt-sdk-${version}-ramips-mt7620_gcc-7.5.0_musl.Linux-x86_64/bin/targets/ramips/mt7620/packages/* ./packages/
 
-make image PROFILE=psg1218a PACKAGES="uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-app-firewall luci-proto-ppp  luci-mod-admin-full luci-theme-bootstrap wpad luci-app-shadowsocks-libev shadowsocks-libev-ss-local -wpad-basic -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only curl libcurl libopenssl openssl-util screen kmod-xmurp-ua" CONFIG_IPV6=n
+make image PROFILE=psg1218a PACKAGES="uhttpd uhttpd-mod-ubus libiwinfo-lua luci-base luci-app-firewall luci-proto-ppp  luci-mod-admin-full luci-theme-bootstrap wpad luci-app-shadowsocks-libev shadowsocks-libev-ss-local -wpad-basic -ip6tables -odhcp6c -kmod-ipv6 -kmod-ip6tables -odhcpd-ipv6only curl openssl-util screen kmod-xmurp-ua" CONFIG_IPV6=n
 
